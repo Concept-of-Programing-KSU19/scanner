7b012027c97c6b7f49c904088a0888fe891b234e
mmkabir031 <56416263+mmkabir031@users.noreply.github.com>
2019-10-16 15:38:54 -0400

    Update Global.java

--- Global.java
+++ Global.java
@@ -1,24 +1,17 @@
 package com.company;
 
 import java.io.*;
-import java.nio.charset.StandardCharsets;
 
-public class Global {
+public class Global
+{
     public static String line;
     public static String [] words;
     public static int counterLine=0;
     public static String space = " ";
-    public static String [] keyWordArr = {"xor", "end","return"};
-    public static String [] symArr = {"/", "\\","+","-","=","*"};
+    public static String [] keyWordArr = {"end","return", "begin" , "while", "if", "for", "try", "return", "break" ,"continue", "function", "macro" ,"quote ","let ","local ","global", "const ","do","struct","end"," else ","catch ","finally"};
+
+    //Add +x and -x(unary plus	the identity operation and unary minus	maps values to their additive inverses)
+    //Add !x negation	changes true to false and vice versa    
+    public static String [] symArr = {"/", "\\","+","-","=","*","==", "!=", "<", ">", "<=", ">=","xor", "^", "%" };
     public static int [] intArr=new int[100];
-    public static int match;
-    //Want these global or not???
-    public static InputStream in;
-    public static InputStreamReader inR;
-    public static BufferedReader br;
-    public static OutputStream out;
-    public static OutputStreamWriter outW;
-    public static BufferedWriter bw;
-    }
-
-
+}
